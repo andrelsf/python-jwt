604dca90a2ce17da8a8b65d1db1ef10404acbe67
Andre Ferreira <andre.ferreira@soluti.com.br>
2020-04-28 14:29:12 -0300

    Refactoring code separeting functions JWT of the run.py

--- auth/run.py
+++ auth/run.py
@@ -34,25 +34,7 @@ def create_tables():
 """
 jwt = JWTManager(app)
 
-"""
-    Using the expired_token_loader decorator
-    will now call this function whenever an expired
-    but otherwise valid access token attemps to access an endpoint
-"""
-@jwt.expired_token_loader
-def expiredTokenCallback(expired_token):
-    token_type = expired_token['type']
-    return {
-        'status': 401,
-        'message': '{} token has expired'.format(token_type)
-    }, 401
-
-
-@jwt.token_in_blacklist_loader
-def check_if_token_in_blacklist(decrypted_token):
-    jti = decrypted_token['jti']
-    return models.RevokedTokenModel.is_jti_blacklisted(jti)
-
+import auth.jwt
 from errors.handler import handleException
 import views, models, resources
 
